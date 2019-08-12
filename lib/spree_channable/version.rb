105f6b9278f617d34186fbe4eea12c176f11a3a7
Fabian Oudhaarlem <fabian@oldharlem.nl>
2019-08-12 12:53:15 +0200

    bump version

--- lib/spree_channable/version.rb
+++ lib/spree_channable/version.rb
@@ -10,7 +10,7 @@ module SpreeChannable
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 17
+    TINY  = 18
     PRE   = 'alpha'.freeze
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
