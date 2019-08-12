a44f173b3af6675d95241cc66bdf56b12a680978
Fabian Oudhaarlem <fabian@oldharlem.nl>
2019-08-12 12:25:53 +0200

    fix dynamic factory definitions for channable setting factory

--- spec/factories/channable_settings.rb
+++ spec/factories/channable_settings.rb
@@ -1,22 +1,22 @@
 FactoryBot.define do
   factory :channable_setting do
-    host { "http://example.com" }
-    url_prefix { "/products" }
-    image_host { "http://example.com" }
-    product_condition { ChannableSetting::PRODUCT_CONDITIONS.sample }
-    brand { "My Brand" }
-    delivery_period { "1 Day" }
-    use_variant_images { false }
-    channable_api_key { "MyString" }
-    company_id { "MyString" }
-    project_id { "MyString" }
+    host "http://example.com"
+    url_prefix "/products"
+    image_host "http://example.com"
+    product_condition ChannableSetting::PRODUCT_CONDITIONS.sample
+    brand "My Brand"
+    delivery_period "1 Day"
+    use_variant_images false
+    channable_api_key "MyString"
+    company_id "MyString"
+    project_id "MyString"
     association :payment_method, factory: :check_payment_method
-    polling_interval { 1 }
+    polling_interval 1
     association :stock_location, factory: :stock_location
-    active { false }
+    active false
 
     trait :active do
-      active { true }
+      active true
     end
   end
 end
