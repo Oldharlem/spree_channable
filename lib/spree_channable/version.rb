f6c6c191c73d3ba7a1b5244139125e1d6a39e9f6
Fabian Oudhaarlem <fabian@oldharlem.nl>
2019-08-20 10:18:06 +0200

    version 0.0.20.alpha

--- lib/spree_channable/version.rb
+++ lib/spree_channable/version.rb
@@ -10,7 +10,7 @@ module SpreeChannable
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 19
+    TINY  = 20
     PRE   = 'alpha'.freeze
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
